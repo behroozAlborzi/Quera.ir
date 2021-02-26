6b9feca9a432371110493fe60734f4a6d7710b97
behroozAlborzi <77720045+behroozAlborzi@users.noreply.github.com>
2021-02-26 20:30:26 +0330

    Update Q5_7.java

--- Q5_7.java
+++ Q5_7.java
@@ -3,20 +3,30 @@ public class Q5_7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long number = scanner.nextInt();
-        long sum;
-        while (number >= 10) {
-            sum = 0;
-            while (number > 0) {
-                sum = sum + (number % 10);
-                number /= 10;
-            }
-            number = sum;
-        }
-        number = number % 9;
-        if (number == 0) {
+//         long sum;
+//         while (number >= 10) {
+//             sum = 0;
+//             while (number > 0) {
+//                 sum = sum + (number % 10);
+//                 number /= 10;
+//             }
+//             number = sum;
+//         }
+//         number = number % 9;
+//         if (number == 0) {
+//             System.out.println(number);
+//         } else {
+//             System.out.println(number);
+//         }
+        
+        // Solution2:
+        
+         number = number % 9;
+        if(number == 0)
+            System.out.println("9");
+        else
             System.out.println(number);
-        } else {
-            System.out.println(number);
-        }
+        
+        scanner.close();
     }
 }
