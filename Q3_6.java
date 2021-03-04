d7980a531fdf8eb5f1532249034391577ef6de1e
behroozAlborzi <77720045+behroozAlborzi@users.noreply.github.com>
2021-03-04 21:19:29 +0330

    Uptade Q3_6 for current

--- Q3_6.java
+++ Q3_6.java
@@ -5,27 +5,16 @@ public class Q3_6 {
         Scanner scanner = new Scanner(System.in);
 
 
-        int a = scanner.nextInt(),b=scanner.nextInt();
-
-        if (a == 0 & b!=0){
-            a = 0;
-            b = 60 -b;
-        }else if(b==0 & a!=0){
-            b = 0;
-            a = 12-a;
-        }else if(b==0 & a==0){
-            a=0;
-            b=0;
-        }
-        else {
-            a = 12 - a;
-            b = 60 - b;
-
-
-
-        }
-        System.out.printf("%02d:%02d", a, b);
-
+       int n=scanner.nextInt();
+        long sum=0;
+        int x=n/2;
+        // age n fard bashe adad ha 0 1 2 ... x-1 x x x-1 ... 1 0 hstn
+        // age n zoj bashe adad ha 0 1 2 ... x-1 x x-1 ... 1 0 hstn
+        sum=(long)(x-1)*x/2;// jame 0 1 2 ... x-1
+        sum*=2;
+        sum+=x*(n%2+1);
+        double ans=(double)sum/(n+1);
+        System.out.printf("%.6f\n",ans);
 
     }
 }
